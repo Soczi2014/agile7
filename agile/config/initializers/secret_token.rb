49d05f0ae3fb487fc2accde7079b9a3dec48a0c8
Radek <radekgiz@gmail.com>
2014-03-24 12:26:32 +0100

    appka

---
+++ agile/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agile::Application.config.secret_key_base = '7428e6585fb7b2a1067b3a4aae3dddcdfa196593a3685593ab5fe1bc2458aa562b1ca12183d2e520b71df6963f6572f156cab9a7a9d5be45e79d1335772eb908'
